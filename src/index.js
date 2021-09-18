796b7d247e3369803ee6d3b5278ff75206eae64d
Sofiyullah abdullah <habsoff@gmail.com>
2021-09-18 12:48:27 +0100

    feat: created Redux store and binding it to component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./Views/App";
+import App from "./Components/App";
 import "semantic-ui-css/semantic.min.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+import middleware from "./Middleware";
+import reducer from "./Reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
